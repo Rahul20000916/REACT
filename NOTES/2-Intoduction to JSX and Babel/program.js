2c7d5c6e9eda15bf5c8d0918b5ca32f4292560b9
unknown <rahulpradeep061@gmail.com>
2024-03-19 10:28:32 +0530

    updated all

---
+++ NOTES/2-Intoduction to JSX and Babel/program.js
@@ -0,0 +1,11 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <h1>what to show it and where to show it</h1>,
+  document.getElementById("root")
+);
+
+var h1 = document.createElement("h1");
+h1.innerHTML = " hello world";
+document.getElementById("root").appendChild(h1);
